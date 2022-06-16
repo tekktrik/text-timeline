39426a923cbb7bcce98e6ae532361d46d14ab5cd
Alec Delaney <tekktrik@gmail.com>
2022-06-16 14:15:18 -0400

    Add prototype code for text_timeline.__init__.py

--- text_timeline/__init__.py
+++ text_timeline/__init__.py
@@ -8,3 +8,26 @@ text_timeline
 
 Main entry for making timelines from formatted text files
 """
+
+from typing import Optional
+import os
+
+class TextTimeline:
+    """Main entry point for creating timelines from files"""
+
+    def __init__(self, filename: str, *, name: Optional[str] = None, **kwargs) -> None:
+
+        self._filename = filename
+        self.nodes = []
+        """The list of timeline nodes"""
+        self.name = name if name else os.path.basename(os.path.splitext(filename))
+
+        self._parse_file(filename)
+
+    @property
+    def filename(self):
+        """The filename of the text file"""
+        return self._filename
+
+    def _parse_file(self, filename: str):
+        """Parses a file to create the timeline"""
