400201ebba55ef3d308a79f105bce9097816680b
Alec Delaney <tekktrik@gmail.com>
2022-06-16 11:33:45 -0400

    Add __init__.py license and module docstring

---
+++ text_timeline/__init__.py
@@ -0,0 +1,10 @@
+# SPDX-FileCopyrightText: 2022 Alec Delaney
+#
+# SPDX-License-Identifier: MIT
+
+"""
+text_timeline
+=============
+
+Main entry for making timelines from formatted text files
+"""
